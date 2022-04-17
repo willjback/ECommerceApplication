ece88f6cd47108e0b25ce3da9f292f31f1ebedd5
Will Back <william.j.back@outlook.com>
2022-04-17 18:52:41 -0500

    CRUD controls added for Admin accounts only

--- Views/HomeView.xaml.cs
+++ Views/HomeView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using ECommerceApplication.Dialog;
@@ -11,6 +14,9 @@ namespace ECommerceApplication.Views
     public partial class HomeView : UserControl
     {
         ECommerceEntities entities = new ECommerceEntities();
+
+        string filepath = @"C:\MSSA\20483\Project\ECommerceApplication\userinfo.txt";
+
         public HomeView()
         {
             InitializeComponent();
@@ -28,6 +34,78 @@ namespace ECommerceApplication.Views
 
             ViewItemDialog productViewer = new ViewItemDialog();
             productViewer.ShowItem((Product)gridProducts.SelectedItem);
+        }
+
+        private void  BtnCreateItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (TxtProductName.Text == string.Empty || TxtProductPrice.Text == string.Empty)
+            {
+                MessageBox.Show("Product Name and Product Price must be filled out", "Item not added", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                Decimal.Parse(TxtProductPrice.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Product Price must only contain integers or decimals", "Item not added", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            Product product = new Product();
+            product.ProductName = TxtProductName.Text;
+            product.Price = Decimal.Parse(TxtProductPrice.Text);
+            entities.Products.Add(product);
+            entities.SaveChanges();
+            RefreshData();
+        }
+
+        private void BtnRemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (gridProducts.SelectedItem == null)
+            {
+                MessageBox.Show("You must click an item in the grid first", "", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            entities.Products.Remove((Product)gridProducts.SelectedItem);
+            entities.SaveChanges();
+            RefreshData();
+        }
+
+        private void Added_Product(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void RefreshData()
+        {
+            gridProducts.ItemsSource = null;
+            gridProducts.ItemsSource = entities.Products.ToList();
+
+            TxtProductName.Clear();
+            TxtProductPrice.Clear();
+
+            if (File.ReadLines(filepath).Skip(3).Take(1).First().ToString() == "True")
+            {
+                AdminGrid.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                AdminGrid.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void gridProducts_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (e.PropertyName == "ProductID")
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void UserControl_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            RefreshData();
         }
     }
 }
